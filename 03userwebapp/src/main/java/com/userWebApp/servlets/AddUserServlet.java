f60e239ddf9127752bd688aa7a9c62e9831d18e9
Manoj M <manumadhu1425@gmail.com>
2023-03-10 13:06:01 +0530

    Adding user to database

---
+++ 03userwebapp/src/main/java/com/userWebApp/servlets/AddUserServlet.java
@@ -0,0 +1,65 @@
+package com.userWebApp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addUserServlet")
+public class AddUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+
+	public void init() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/newdb", "root", "root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		try (Statement statement = connection.createStatement();) {
+			int result = statement.executeUpdate("insert into users values('" + firstname + "','" + lastname + "','"
+					+ email + "','" + password + "')");
+			PrintWriter out = response.getWriter();
+			if (result > 0) {
+				out.println("<h1>User created in db</h1>");
+			} else {
+				out.println("<h1>Error in creating user</h1>");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+		try {
+			if (connection != null)
+				connection.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+	}
+
+}

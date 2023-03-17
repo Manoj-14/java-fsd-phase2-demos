b78af2fd1581401d909ffd4db202566b5d9820bd
Manoj M <manumadhu1425@gmail.com>
2023-03-17 10:44:27 +0530

    Servlet to get request and to send response

---
+++ 11StudentWebapp/src/main/java/com/project/resources/servlets/StudentsList.java
@@ -0,0 +1,63 @@
+package com.project.resources.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.project.utils.StudentsDataUtils;
+
+@WebServlet("/StudentsList")
+public class StudentsList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	StudentsDataUtils studentsDataUtils;
+
+	public void init(ServletConfig config) {
+
+		try {
+			ServletContext context = config.getServletContext();
+			String dburl = context.getInitParameter("dburl");
+			String dbuser = context.getInitParameter("dbuser");
+			String dbpassword = context.getInitParameter("dbpassword");
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(dburl, dbuser, dbpassword);
+			studentsDataUtils = new StudentsDataUtils(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("students", studentsDataUtils.getStudents());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view_students.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void destroy() {
+		try {
+			if (connection != null)
+				connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

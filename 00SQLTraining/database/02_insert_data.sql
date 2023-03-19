8cc352728fa43396c712b39cf0f66d5824698471
Manoj M <manumadhu1425@gmail.com>
2023-03-19 18:14:31 +0530

    Inserting data

---
+++ 00SQLTraining/database/02_insert_data.sql
@@ -0,0 +1,6 @@
+use newdb;
+
+
+insert into person(firstname,lastname,dob,notes) values("Manoj","M","2001-08-14","Joined");
+insert into person(firstname,lastname,dob,notes) values("Mohan","V","2001-10-23","Helll0");
+insert into person(firstname,lastname,dob) values("Darshan","V","2002-03-18");

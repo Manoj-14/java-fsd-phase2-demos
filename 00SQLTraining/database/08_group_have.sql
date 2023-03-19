d3691e9850b4899742161021f6c0bb37ac65f081
Manoj M <manumadhu1425@gmail.com>
2023-03-19 22:58:21 +0530

    Group and have

--- 00SQLTraining/database/08_group_have.sql
+++ 00SQLTraining/database/08_group_have.sql
@@ -10,7 +10,7 @@ select left(firstname,1) initial,salary,count(*),min(salary),max(salary)
 from person
 group by initial,salary;
 
-select left(firstname,1) initial,salary,count(*),min(salary),max(salary)
+select left(firstname,1) initial,count(salary),min(salary),max(salary)
 from person
 group by initial,salary
 having salary>1000;

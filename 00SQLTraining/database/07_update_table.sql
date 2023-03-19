7094486879c23b8e2978d1f54e5779f821eaf6be
Manoj M <manumadhu1425@gmail.com>
2023-03-19 18:44:22 +0530

    Update table person

---
+++ 00SQLTraining/database/07_update_table.sql
@@ -0,0 +1,12 @@
+update person
+set salary = 15000
+where firstname ="Manoj";
+
+
+update person
+set salary = 20000
+where firstname ="Darshan";
+
+update person
+set salary = 10000
+where firstname ="Mohan";

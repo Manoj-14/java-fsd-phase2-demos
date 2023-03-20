5f932d709ad627201874e04dca97498b3ab795e2
Manoj M <manumadhu1425@gmail.com>
2023-03-20 12:38:14 +0530

    Added data to db using hibernate annotation by removing mapping files

---
+++ 14HibernateHelloWorldAnnotations/src/com/hibernateDemo/HibernateDemo.java
@@ -0,0 +1,17 @@
+package com.hibernateDemo;
+
+import com.hibernateDemo.domine.Message;
+import com.hibernateDemo.utils.HibernateUtil;
+import org.hibernate.Session;
+
+public class HibernateDemo {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Message message = new Message("Second mesage from hibernate annotations");
+		
+		session.save(message);
+		session.getTransaction().commit();
+		session.close();
+	}
+}

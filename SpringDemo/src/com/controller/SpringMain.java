7cca80bcc0da1db55089756d9efadff2a48686a3
Manoj M <manumadhu1425@gmail.com>
2023-03-22 11:13:49 +0530

    Multiple setter injection by mentioning id

--- SpringDemo/src/com/controller/SpringMain.java
+++ SpringDemo/src/com/controller/SpringMain.java
@@ -8,7 +8,9 @@ import com.transferobject.Hello;
 public class SpringMain {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		Hello h = context.getBean(Hello.class);
-		System.out.println(h.getMessage());
+		Hello h1 = (Hello) context.getBean("h1");
+		Hello h2 = (Hello) context.getBean("h2");
+		System.out.println(h1.getMessage());
+		System.out.println(h2.getMessage());
 	}
 }
